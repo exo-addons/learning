cf741fe5f2e41ee59f3e88646f59e312c2a1f70d
Ali Hamdi <ahamdi@exoplatform.com>
2021-03-12 11:37:24 +0100

    Reinitialize project from scratch

--- e-learning-services/src/main/java/org/exoplatform/addon/elearning/storage/CategoryDao.java
+++
@@ -1,9 +0,0 @@
-package org.exoplatform.addon.elearning.storage;
-
-import org.exoplatform.addon.elearning.entities.CategoryEntity;
-import org.exoplatform.commons.persistence.impl.GenericDAOJPAImpl;
-
-
-public class CategoryDao  extends GenericDAOJPAImpl<CategoryEntity,Long> {
-
-}
