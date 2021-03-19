adc6386e0ba5f990be429e9180493f955cf42eaf
Assil <assil.hamdi@esprit.tn>
2021-03-19 13:54:32 +0100

    CourseService updated & configured

--- e-learning-services/src/main/java/org/exoplatform/addon/elearning/service/configuration/CourseService.java
+++ e-learning-services/src/main/java/org/exoplatform/addon/elearning/service/configuration/CourseService.java
@@ -1,38 +1,56 @@
 package org.exoplatform.addon.elearning.service.configuration;
 
-
-import org.exoplatform.addon.elearning.storage.CourseDao;
-import org.exoplatform.addon.elearning.entities.CourseEntity;
-
-import java.util.logging.Logger;
 import java.util.List;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
-
-import org.json.JSONObject;
 import org.json.JSONArray;
+import org.json.JSONObject;
 
-public class CourseService {
+import org.exoplatform.addon.elearning.entities.CourseEntity;
+import org.exoplatform.addon.elearning.storage.CourseDao;
+import org.exoplatform.container.PortalContainer;
+import org.exoplatform.services.rest.resource.ResourceContainer;
 
-  private CourseDao courseDao;
 
+@Path("course")
+@Produces("application/json")
+public class CourseService implements ResourceContainer {
+  
+  
+  private PortalContainer container = PortalContainer.getInstance();
+  private CourseDao courseDao = (CourseDao)container.getComponentInstanceOfType(CourseDao.class);
+  
+  
+  //private CourseDao courseDao;
+  
   public CourseService(CourseDao courseDao) {
     this.courseDao = courseDao;
   }
-
+  
+  
   @GET
+  @Path("/test")
+  public String hello() {
+    return "Hello";
+  }
+
+  @POST
   @Path("/addCourse")
   public Response addCourse(@Context UriInfo uriInfo,
-                             @QueryParam("courseTitle") String courseTitle,
-                             @QueryParam("courseDescription") String courseDescription) throws Exception {
+                            @QueryParam("courseTitle") String courseTitle,
+                            @QueryParam("courseDescription") String courseDescription) throws Exception {
     CourseEntity course = new CourseEntity(courseTitle, courseDescription);
     courseDao.create(course);
     JSONObject courseJson = new JSONObject();
@@ -40,24 +58,19 @@ public class CourseService {
     courseJson.put("title", course.getCourseTitle());
     courseJson.put("description", course.getCourseDescription());
 
-    Logger LOGGER = Logger.getLogger("LOG");
-    LOGGER.info("Added");
-    
     return Response.created(uriInfo.getAbsolutePath())
                    .entity(courseJson.toString())
                    .type(MediaType.TEXT_PLAIN + "; charset=utf-8")
                    .status(Response.Status.OK)
                    .build();
   }
-  
-  
-  
-  @GET
+
+  @PUT
   @Path("/updateCourse/{courseId}")
   public Response updateCourse(@Context UriInfo uriInfo,
-                                @PathParam("courseId") String courseId,
-                                @QueryParam("courseTitle") String courseTitle,
-                                @QueryParam("courseDescription") String courseDescription) throws Exception {
+                               @PathParam("courseId") String courseId,
+                               @QueryParam("courseTitle") String courseTitle,
+                               @QueryParam("courseDescription") String courseDescription) throws Exception {
     CourseEntity course = courseDao.find(Long.parseLong(courseId));
     if (course != null) {
       course.setCourseTitle(courseTitle);
@@ -79,9 +92,8 @@ public class CourseService {
                    .status(Response.Status.NOT_FOUND)
                    .build();
   }
-  
-  
-  @GET
+
+  @DELETE
   @Path("/deleteCourse/{courseId}")
   public Response deleteCourse(@Context UriInfo uriInfo, @PathParam("courseId") String courseId) throws Exception {
     CourseEntity course = courseDao.find(Long.parseLong(courseId));
@@ -103,8 +115,7 @@ public class CourseService {
                    .status(Response.Status.NOT_FOUND)
                    .build();
   }
-  
-  
+
   @GET
   @Path("/getAllCourses")
   public Response getAllCourses(@Context UriInfo uriInfo) throws Exception {
@@ -123,8 +134,7 @@ public class CourseService {
                    .status(Response.Status.OK)
                    .build();
   }
-  
-  
+
   @GET
   @Path("/getCourseById/{courseId}")
   public Response getCourseById(@Context UriInfo uriInfo, @PathParam("courseId") String courseId) throws Exception {
