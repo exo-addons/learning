13747f778e8b3020dda285cfb2da9df70b1b78be
Moez Diha <mdiha@exoplatform.com>
2021-08-13 14:22:45 +0100

    Task-03: Add entities.

---
+++ e-learning-services/src/main/java/org/exoplatform/addon/elearning/entities/TutorialEntity.java
@@ -0,0 +1,141 @@
+package org.exoplatform.addon.elearning.entities;
+
+import org.exoplatform.commons.api.persistence.ExoEntity;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Collection;
+
+@Entity
+@ExoEntity
+@Table(name = "ADDON_E_LEARNING_TUTO")
+@NamedQueries({
+        @NamedQuery(name = "TutorialEntity.getAllTutosByTheme", query = "SELECT t FROM TutorialEntity t INNER JOIN t.themeIds theme WHERE theme = :id"),
+        @NamedQuery(name = "TutorialEntity.findTutosByName", query = "SELECT DISTINCT t FROM TutorialEntity t INNER JOIN t.themeIds theme WHERE theme = :id AND LOWER(t.title) LIKE LOWER(CONCAT('%', :tutoTitle, '%'))")})
+public class TutorialEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "ID")
+  private Long id;
+
+  @Column(name = "TITLE")
+  private String title;
+
+  @Column(name = "DESCRIPTION")
+  private String description;
+
+  @Column(name = "AUTHOR")
+  private String author;
+
+  @Column(name = "CREATED_DATE")
+  private Timestamp createdDate;
+
+  @ElementCollection
+  @CollectionTable(name = "ADDON_E_LEARNING_TUTO_THEME", joinColumns = @JoinColumn(name = "TUTO_ID"))
+  @Column(name = "THEME_ID")
+  public Collection<Long> themeIds;
+
+  /*
+   * @OneToMany(mappedBy = "tuto", cascade = CascadeType.ALL, fetch =
+   * FetchType.LAZY) private Collection<StepEntity> steps;
+   */
+
+  @Column(name = "STATUS")
+  private String status;
+
+  public TutorialEntity() {
+  }
+
+  public TutorialEntity(Long id,
+                        String title,
+                        String description,
+                        String author,
+                        Timestamp createdDate,
+                        Collection<Long> themeIds,
+                        String status) {
+
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.author = author;
+    this.createdDate = createdDate;
+    this.themeIds = themeIds;
+    this.status = status;
+  }
+
+  public TutorialEntity(String title,
+                        String description,
+                        String author,
+                        Timestamp createdDate,
+                        Collection<Long> themeIds,
+                        String status) {
+    this.title = title;
+    this.description = description;
+    this.author = author;
+    this.createdDate = createdDate;
+    this.themeIds = themeIds;
+    this.status = status;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+
+  public Timestamp getCreatedDate() {
+    return createdDate;
+  }
+
+  public void setCreatedDate(Timestamp createdDate) {
+    this.createdDate = createdDate;
+  }
+
+  public Collection<Long> getThemeIds() {
+    return themeIds;
+  }
+
+  public void setThemeIds(Collection<Long> themeIds) {
+    this.themeIds = themeIds;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  public enum Status {
+    DRAFT, ARCHIVED, PUBLISHED
+  }
+
+}
