bc0cc57a64b662ec500ff47207c562b312dc2b11
Moez Diha <mdiha@exoplatform.com>
2021-08-13 16:50:42 +0100

    Task-03: Add templates for Themes.

---
+++ e-learning-webapps/src/main/webapp/vue-app/components/Tutorials/TutorialDashboard.vue
@@ -0,0 +1,150 @@
+<template>
+  <v-app
+    id="tutorials_dashboard"
+    flat>
+    <tutorial-dashboard-toolbar 
+      :theme-name="this.themeName"
+      :keyword="keyword"
+      @keyword-changed="keyword = $event" />
+    <tutorial-card-list
+      :keyword="keyword" />
+    <exo-confirm-dialog
+      ref="confirmDialog"
+      :message="$t('addon.elearning.tutorial.deleteConf')"
+      :title="$t('addon.elearning.tutorial.confirmD')"
+      :ok-label="$t('addon.elearning.tutorial.confirm')"
+      :cancel-label="$t('addon.elearning.tutorial.cancel')"
+      @ok="deleteTuto()" />
+    <template>
+      <div class="text-center">
+        <v-snackbar
+          v-model="successBar"
+          :timeout="timeout"
+          :color="this.color">
+          {{ text }}
+          <template v-slot:action="{ attrs }">
+            <v-btn 
+              class="snackbar_btn"
+              text
+              v-bind="attrs"
+              @click="successBar = false">
+              {{ $t('addon.elearning.tutorial.dismiss') }}
+            </v-btn>
+          </template>
+        </v-snackbar>
+      </div>
+    </template>   
+  </v-app>
+</template>
+
+<script>
+export default {
+  
+  data() {
+    return {
+      successBar: false,
+      text: '',
+      color: '',
+      timeout: 0,
+      deleteId: null,
+      themeId: null,
+      themeName: null,
+      errors: [],
+      keyword: null
+    };
+  },
+
+  created() {
+    this.$root.$on('tutoCreated', () => {
+      this.timeout=3000;
+      this.successBar=false;
+      this.successBar=true;
+      this.color='success';
+      this.text=this.$t('addon.elearning.tutorial.created');
+    });
+    this.$root.$on('tutoUpdated', () => {
+      this.timeout=3000;
+      this.successBar=false;
+      this.successBar=true;
+      this.color='success';
+      this.text=this.$t('addon.elearning.tutorial.updated');
+    });
+    this.$root.$on('tutoMoved', () => {
+      this.timeout=3000;
+      this.successBar=false;
+      this.successBar=true;
+      this.color='success';
+      this.text=this.$t('addon.elearning.tutorial.moved');
+    });
+    this.$root.$on('tutoDuplicated', () => {
+      this.timeout=3000;
+      this.successBar=false;
+      this.successBar=true;
+      this.color='success';
+      this.text=this.$t('addon.elearning.tutorial.duplicated');
+    });
+    this.$root.$on('tutoArchived', () => {
+      this.timeout=3000;
+      this.successBar=false;
+      this.successBar=true;
+      this.color='success';
+      this.text=this.$t('addon.elearning.tutorial.archived');
+    });
+    this.$root.$on('tutoUnarchived', () => {
+      this.timeout=3000;
+      this.successBar=false;
+      this.successBar=true;
+      this.color='success';
+      this.text=this.$t('addon.elearning.tutorial.unarchived');
+    });
+    this.$root.$on('setId', (id) => {
+      this.themeId=id;
+      this.getThemeName();
+    });
+    this.$root.$on('deleteTuto', (id) => {
+      this.prepareDelete(id);
+    });
+    this.$root.$on('makeTuto', () => {
+      this.$root.$emit('addTuto');
+    });
+    this.$root.$on('backtoThemes', () => {
+      this.$root.$emit('backThemes');
+    });
+
+    $(document).on('mousedown', () => {
+      if (this.displayActionMenu) {
+        window.setTimeout(() => {
+          this.displayActionMenu = false;
+        }, 200);
+      }
+    });
+
+    this.$root.$on('key-changed', (keyword) => {
+      this.keyword=keyword;
+    });
+
+  },
+
+  methods: {
+    prepareDelete(id){
+      this.deleteId=id;
+      this.$refs.confirmDialog.open();
+    },
+    deleteTuto() {
+      return this.$tutoService.deleteTuto(this.deleteId)
+        .then(() => {
+          this.confirmDialog=false;
+          this.timeout=3000;
+          this.successBar=true;
+          this.text=this.$t('addon.elearning.tutorial.deleted');
+          this.$root.$emit('tutoDeleted');})
+        .catch((e) => this.errors.push(e));
+    },
+    getThemeName() {
+      return this.$themeService.getThemeById(this.themeId)
+        .then((data) => {this.themeName= data.name;})
+        .catch((e) => this.errors.push(e));
+    }
+  }  
+};
+</script>
