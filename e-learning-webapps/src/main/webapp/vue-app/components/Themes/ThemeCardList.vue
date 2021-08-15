1bc4f941e24613b387d24f8b6a8bc104dfb9e2e9
Moez Diha <mdiha@exoplatform.com>
2021-08-16 00:31:53 +0100

    Add vuejs templates for themes.

---
+++ e-learning-webapps/src/main/webapp/vue-app/components/Themes/ThemeCardList.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-app
+    id="themes_card_list"
+    flat>
+    <template>
+      <v-container> 
+        <v-row class="theme_cards_row border-box-sizing"> 
+          <v-col
+            class="theme_cards"
+            cols="12"
+            md="6"
+            lg="4"
+            xl="3"
+            v-for="theme in themeList"
+            :key="theme.id"
+            :id="'theme-'+theme.id">
+            <theme-card :theme="theme" />
+          </v-col>
+        </v-row>
+      </v-container>  
+    </template> 
+  </v-app>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return {
+      themeList: [],
+      errors: [],
+    };
+  },
+  props: {
+    keyword: {
+      type: String,
+      default: null,
+    }
+  },
+  
+  watch: {
+    keyword() {
+      if (!this.keyword) {
+        this.getThemes();
+      }
+      if (this.keyword) {
+        this.findThemes();
+      }
+
+    }
+  },
+
+  created() {
+    this.getThemes();
+    this.$root.$on('themeCreated', () => {
+      this.getThemes();
+    });
+    this.$root.$on('themeUpdated', () => {
+      this.getThemes();
+    });
+    this.$root.$on('themeDeleted', () => {
+      this.getThemes();
+    });
+  },
+  
+  methods: {
+    getThemes() {
+      return this.$themeService.getThemes(this.keyword)
+        .then((data) => {
+          this.themeList.splice(0);
+          this.themeList = data;})
+        .catch((e) => this.errors.push(e));
+    },
+    findThemes(){
+      return this.$themeService.getThemesByName(this.keyword)
+        .then((data) => {
+          this.themeList.splice(0);
+          this.themeList = data;})
+        .catch((e) => this.errors.push(e));
+    }
+  }  
+};
+</script>
---
+++ e-learning-webapps/src/main/webapp/vue-app/components/Themes/ThemeDashboard.vue
@@ -0,0 +1,107 @@
+<template>
+  <v-app
+    id="themes_dashboard"
+    flat>
+    <theme-dashboard-toolbar
+      :keyword="keyword"
+      @keyword-changed="keyword = $event" />
+    <theme-card-list 
+      :keyword="keyword" />    
+    <exo-confirm-dialog
+      ref="confirmDialog"
+      :message="$t('addon.elearning.theme.deleteConf')"
+      :title="$t('addon.elearning.tutorial.confirmD')"
+      :ok-label="$t('addon.elearning.tutorial.confirm')"
+      :cancel-label="$t('addon.elearning.tutorial.cancel')"
+      @ok="deleteTheme()" />
+    <template>
+      <div class="text-center">
+        <v-snackbar
+          v-model="successBar"
+          :timeout="timeout"
+          :color="this.color">
+          {{ text }}
+          <template v-slot:action="{ attrs }">
+            <v-btn 
+              class="snackbar_btn"
+              text
+              v-bind="attrs"
+              @click="successBar = false">
+              {{ $t('addon.elearning.tutorial.dismiss') }}
+            </v-btn>
+          </template>
+        </v-snackbar>
+      </div>
+    </template>   
+  </v-app>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return {
+      successBar: false,
+      text: '',
+      color: 'success',
+      timeout: 0,
+      deleteId: null,
+      errors: [],
+      keyword: null
+    };
+  },
+
+  created() {
+    this.$root.$on('themeCreated', () => {
+      this.timeout=3000;
+      this.successBar=true;
+      this.text=this.$t('addon.elearning.theme.created');
+    });
+    this.$root.$on('themeUpdated', () => {
+      this.timeout=3000;
+      this.successBar=true;
+      this.text=this.$t('addon.elearning.theme.updated');
+    });
+    this.$root.$on('deleteTheme', (id) => {
+      this.prepareDelete(id);
+    });
+    this.$root.$on('makeTheme', () => {
+      this.$root.$emit('addTheme');
+    });
+    this.$root.$on('makeUpdateTheme', (id) => {
+      this.$root.$emit('updateTheme', id);
+    });
+    this.$root.$on('makeShowTutos', (id) => {
+      this.$root.$emit('showTutos', id);
+    });
+    this.$on('keyword-changed', (keyword) => {
+      this.keyword=keyword;
+    });
+
+  },
+
+  methods: {
+    prepareDelete(id){
+      this.deleteId=id;
+      this.$refs.confirmDialog.open();
+    },
+    deleteTheme() {
+      return this.$themeService.deleteTheme(this.deleteId)
+        .then(() => {
+          this.confirmDialog=false;
+          this.successBar=true;
+          this.color='success';
+          this.timeout=3000;
+          this.text=this.$t('addon.elearning.theme.deleted');
+          this.$root.$emit('themeDeleted');
+        })
+        .catch((e) => {console.error('Error deleting theme', e);
+          this.confirmDialog=false;
+          this.successBar=true;
+          this.color='error';
+          this.timeout=5000;
+          this.text=this.$t('addon.elearning.theme.delete.fail');});
+    }
+  }  
+};
+</script>
