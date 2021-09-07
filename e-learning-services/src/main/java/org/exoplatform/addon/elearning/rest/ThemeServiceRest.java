153164c581f5d17c7dccfa4f2235267f96f5b383
Moez Diha <mdiha@exoplatform.com>
2021-09-07 18:53:09 +0100

    Code refactor.

--- e-learning-services/src/main/java/org/exoplatform/addon/elearning/rest/ThemeServiceRest.java
+++ e-learning-services/src/main/java/org/exoplatform/addon/elearning/rest/ThemeServiceRest.java
@@ -1,9 +1,12 @@
 package org.exoplatform.addon.elearning.rest;
 
+import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.apache.commons.lang3.StringUtils;
-import org.exoplatform.addon.elearning.service.ThemeService;
 import org.exoplatform.addon.elearning.dto.Theme;
+import org.exoplatform.addon.elearning.service.ThemeService;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
 import org.exoplatform.services.rest.resource.ResourceContainer;
@@ -11,12 +14,10 @@ import org.exoplatform.services.security.ConversationState;
 import org.exoplatform.social.core.space.model.Space;
 import org.exoplatform.social.core.space.spi.SpaceService;
 
+import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Path("theme")
@@ -37,8 +38,12 @@ public class ThemeServiceRest implements ResourceContainer {
 
   @POST
   @Path("/addTheme")
-  /* @RolesAllowed("users") */
-  public Response addTheme(Theme theme) {
+  @RolesAllowed("users")
+  @ApiOperation(value = "Adds a theme", httpMethod = "POST", response = Response.class, notes = "This Adds theme")
+  @ApiResponses(value = {@ApiResponse(code = 200, message = "Request fulfilled"),
+          @ApiResponse(code = 400, message = "Invalid query input"), @ApiResponse(code = 403, message = "Unauthorized operation"),
+          @ApiResponse(code = 404, message = "Resource not found")})
+  public Response addTheme(@ApiParam(value = "Theme to save", required = true) Theme theme) {
     try {
       String currentUser = ConversationState.getCurrent().getIdentity().getUserId();
       if (currentUser == null) {
@@ -67,23 +72,33 @@ public class ThemeServiceRest implements ResourceContainer {
 
   @DELETE
   @Path("/deleteTheme/{id}")
-  /* @RolesAllowed("users") */
-  public Response deleteTheme(@PathParam("id") Long id) {
+  @RolesAllowed("users")
+  @ApiOperation(value = "Delete theme", httpMethod = "DELETE", response = Response.class, notes = "This deletes the theme", consumes = "application/json")
+  @ApiResponses(value = {@ApiResponse(code = 200, message = "Theme deleted"),
+          @ApiResponse(code = 400, message = "Invalid query input"),
+          @ApiResponse(code = 401, message = "User not authorized to delete the Project"),
+          @ApiResponse(code = 500, message = "Internal server error")})
+  public Response deleteTheme(@ApiParam(value = "themeId", required = true) @PathParam("id") Long themeId) {
 
     try {
-      themeService.deleteTheme(id);
+      themeService.deleteTheme(themeId);
 
     } catch (Exception e) {
-      LOG.error("Could not deleteThemeById Theme with the Id {}", id, e);
+      LOG.error("Could not deleteThemeById Theme with the Id {}", themeId, e);
       return Response.serverError().entity(e.getMessage()).build();
     }
-    return Response.status(Response.Status.OK).entity("Theme with id " + id + " Deleted").build();
+    return Response.status(Response.Status.OK).entity("Theme with id " + themeId + " Deleted").build();
   }
 
   @PUT
   @Path("/updateTheme")
-  /* @RolesAllowed("users") */
-  public Response updateTheme(Theme theme) {
+  @Produces(MediaType.APPLICATION_JSON)
+  @RolesAllowed("users")
+  @ApiOperation(value = "Update Theme", httpMethod = "PUT", response = Response.class, notes = "This Update Theme info")
+  @ApiResponses(value = {@ApiResponse(code = 200, message = "Request fulfilled"),
+          @ApiResponse(code = 400, message = "Invalid query input"), @ApiResponse(code = 403, message = "Unauthorized operation"),
+          @ApiResponse(code = 404, message = "Resource not found")})
+  public Response updateTheme(@ApiParam(value = "Theme to update", required = true) Theme theme) {
 
     try {
       theme = themeService.updateTheme(theme);
