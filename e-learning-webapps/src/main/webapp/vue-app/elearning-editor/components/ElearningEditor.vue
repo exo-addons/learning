8b770138d36d9f7ea2ed58cf535f89f3c1983de5
Moez Diha <mdiha@exoplatform.com>
2021-10-31 12:57:34 +0100

    Create pae for elearning editor.

---
+++ e-learning-webapps/src/main/webapp/vue-app/elearning-editor/components/ElearningEditor.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Hello Elearning Editor !</h1>
+</template>
+
+<script>
+export default {
+};
+</script>
---
+++ e-learning-webapps/src/main/webapp/vue-app/elearning/main.js
@@ -0,0 +1,36 @@
+import './initComponents.js';
+
+import * as tutoService from '../js/tutoService';
+import * as themeService from '../js/themeService';
+
+Vue.use(Vuetify);
+const vuetify = new Vuetify({
+  dark: true,
+  iconfont: '',
+});
+
+// getting language of user
+const lang = eXo && eXo.env && eXo.env.portal && eXo.env.portal.language || 'en';
+
+const url = `${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/locale.addon.elearning-${lang}.json`;
+
+window.Object.defineProperty(Vue.prototype, '$tutoService', {
+  value: tutoService,
+});
+
+window.Object.defineProperty(Vue.prototype, '$themeService', {
+  value: themeService,
+});
+
+// getting locale ressources
+export function init() {
+  exoi18n.loadLanguageAsync(lang, url)
+    .then(i18n => {
+    // init Vue app when locale ressources are ready
+      new Vue({
+        template: '<elearning-management></elearning-management>',
+        i18n,
+        vuetify,
+      }).$mount('#elearning_app');
+    });
+}
