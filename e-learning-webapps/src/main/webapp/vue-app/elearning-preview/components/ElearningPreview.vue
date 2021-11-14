60561d6e8606cd011167cca082d8de7e8c3654f8
Moez Diha <38471028+mdiha@users.noreply.github.com>
2021-11-14 23:41:40 +0100

    Task-51299: Add Elearning Preview page. (#41)

---
+++ e-learning-webapps/src/main/webapp/vue-app/elearning-preview/components/ElearningPreview.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <div id="ElearningPreview">
+      <h1>Hello Elearning Preview !</h1>
+    </div>
+    
+  </v-app>
+</template>
+
+<script>
+export default {
+};
+</script>
---
+++ e-learning-webapps/src/main/webapp/vue-app/elearning-preview/initComponents.js
@@ -0,0 +1,19 @@
+import ElearningPreview from './components/ElearningPreview.vue';
+
+const components = {
+  'elearning-preview': ElearningPreview,
+};
+
+for (const key in components) {
+  Vue.component(key, components[key]);
+}
+
+//get overrided components if exists
+if (extensionRegistry) {
+  const components = extensionRegistry.loadComponents('ElearningEditor');
+  if (components && components.length > 0) {
+    components.forEach(cmp => {
+      Vue.component(cmp.componentName, cmp.componentOptions);
+    });
+  }
+}
